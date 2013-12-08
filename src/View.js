ac31d91c174255fe0b73001103bbcdeb089d3be2
威老 <qqq123026689@126.com>
2013-12-08 15:45:05 +0800

    remove some comments

--- src/View.js
+++ src/View.js
@@ -1,28 +1,6 @@
-// # S.View 类 #
 // # Strongerbone View #
-// 扩展了 Backbone.View 的一些基本功能。
-//
-// ## 主要特性 ##
-// 1. 渲染的时候自动使用模板引擎渲染。
-// 2. model 改变，视图自动更新。
-// 3. 添加 appendTo 配置项，可以指定要添加到哪个 Dom 下面。
-// 4. 增加子类可以定义并在 afterRender 方法中进行 Dom 操作。
-//
-// ## 示例： ##
-//
-// ### 初始 html 页面 ###
-// `<div id="header"></div>`
-//
-// ### js 脚本 ###
-// var MyView = S.View.extend({
-//    appendTo: '#header',
-//    template: '<div>{{= it.name}}</div>',
-// });
-// var myView = new MyView({data: {name: "weilao"}});
-// myView.render();
-//
-// ### 渲染结果 ###
-// `<div id="header"><div>weilao</div></div>`
+// author: weilao
+// @ https://github.com/weilao
 (function (_window) {
     // 缓存节约模板编译时间。
     var _compiledTplCache = [];
@@ -40,19 +18,6 @@
         hidden: false,
 
         constructor: function (opts) {
-            // opts.appendTo 和 this.appendTo 的差异
-            // -------------------
-            // XXView = BaseView.extend({
-            //     appendTo: '#login' // 这里给出了 this.appendTo
-            // });
-            // new XXView({
-            //     appendTo: 'body'  // 这里给出了 opts.appendTo
-            // });
-            //
-            // 总结
-            // -------------------
-            // this.xx 是在定义视图类的时候给的
-            // opts.xx 是在实例化的时候给的
             opts = opts || {};
             this.template = opts.template || this.template || '';
             this.model = opts.model || this.model || null;
